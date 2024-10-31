6eb8306675d4fa102a5b51eab2fa9b7ae1e27b78
Poul1337 <116201026+Poul1337@users.noreply.github.com>
2024-10-31 20:16:30 +0100

    initial commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  email: string;
+}
